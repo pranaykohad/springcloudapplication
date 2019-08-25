73b2b0f8bce70c3a66c8776db23ad636290e75dd
pranaykohad <prakohad@gmail.com>
2019-08-25 14:42:00 +0530

    ok

---
+++ EurekaClientDemo/src/main/java/com/example/demo/EurekaClientDemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication  
+@EnableDiscoveryClient
+public class EurekaClientDemoApplication {
+	
+	@Value("${service.instance.name}")
+	private String instance;
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientDemoApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String message() {
+		return "Hello form" + instance;
+	}
+
+}
+
